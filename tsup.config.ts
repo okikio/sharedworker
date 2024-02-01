3f1a376e6c0f8ba1b49560f356c67d667b6c3c9b
Okiki <okikio.dev@gmail.com>
2024-02-01 19:50:24 +0000

    fix: add back support for umd #7
    Signed-off-by: Okiki <okikio.dev@gmail.com>

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,6 +1,12 @@
+import type { Options } from 'tsup';
 import { defineConfig } from 'tsup';
+import { umdWrapper } from 'esbuild-plugin-umd-wrapper';
 
-export default defineConfig({
+import { umd } from "./package.json";
+
+const GLOBAL_NAME = umd;
+
+const baseConfig: Options = {
   target: ["es2022", "node21", "chrome105"],
   entry: ['src/index.ts'],
   format: ["esm", "cjs"],
@@ -8,4 +14,24 @@ export default defineConfig({
   clean: true,
   dts: true,
   outDir: "lib",
-})
+  platform: 'browser',
+  globalName: GLOBAL_NAME,
+
+  outExtension({ format, options }) {
+    const ext = ({ "esm": "js", "cjs": "cjs", "umd": "umd.js" })[format]
+    const outputExtension = options.minify ? `min.${ext}` : `${ext}`
+    return {
+      js: `.${outputExtension}`,
+    }
+  },
+}
+
+export default defineConfig([
+  { ...baseConfig },
+  {
+    ...baseConfig,
+    target: 'es5',
+    format: ['umd'],
+    esbuildPlugins: [umdWrapper({ libraryName: GLOBAL_NAME, external: 'inherit' })],
+  },
+])
